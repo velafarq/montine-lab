682298ab828f8a90985137a051a1374925d10496
velafarq <vela.mrdjen@gmail.com>
2018-11-04 17:03:50 -0600

    Add all people to who we are section

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-import { home } from "./js/home.template";
 $(function() {
   $(".menu-bars").click(function() {
     $(".header__nav__list").toggleClass("collapse-menu");
   });
 });
+
+// $('a[href^="#"]').on("click", function(event) {
+//   var target = $(this.getAttribute("href"));
+//   if (target.length) {
+//     event.preventDefault();
+//     $("html, body")
+//       .stop()
+//       .animate(
+//         {
+//           scrollTop: target.offset().top
+//         },
+//         1000
+//       );
+//   }
+// });
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
